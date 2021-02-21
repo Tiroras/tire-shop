6d843479e6d0164f02343bad23c1297fc8b19190
Tiroras <shnyrov2000@gmail.com>
2021-02-21 14:28:30 +0400

    final correction

--- src/components/catalog/CatalogTabs.tsx
+++ src/components/catalog/CatalogTabs.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Link from "next/link";
 import {ICatalog} from "../../interfaces/reducers/INav";
-import styled from "styled-components";
+import classes from "./CatalogTabs.module.css";
 
 
 interface IProps {
@@ -9,66 +9,30 @@ interface IProps {
   className?: string;
 }
 
-const Div = (props) => {
-  return(
-    <div className={props.className}>
-      {props.children}
-    </div>
-  )
-}
-
-const Tabs = styled(Div)`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  flex-wrap: wrap;
-  
-  overflow: hidden;
-`;
-
-const Tab = styled(Div)`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  width: 100%;
-  height: 20vh;
-  background-color: #3E94D1;
-  color: white;
-  font-size: 4vh;
-  overflow: hidden;
-  transition: 0.3s;
-  :hover{
-    background-color: #0a64a4;
-    transition: 0.3s;
-    height: 30vh;
-    font-size: 6vh;
-  }
-`;
-
 const CatalogTabs = (props: IProps) => {
   return(
-    <Tabs>
+    <div className={classes.tabs}>
       <Link href={props.data.tires.sectionLink}>
         <a>
-          <Tab>{props.data.tires.sectionName}</Tab>
+          <div className={classes.tab}>{props.data.tires.sectionName}</div>
         </a>
       </Link>
       <Link href={props.data.disks.sectionLink}>
         <a>
-          <Tab>{props.data.disks.sectionName}</Tab>
+          <div className={classes.tab}>{props.data.disks.sectionName}</div>
         </a>
       </Link>
       <Link href={props.data.mats.sectionLink}>
         <a>
-          <Tab>{props.data.mats.sectionName}</Tab>
+          <div className={classes.tab}>{props.data.mats.sectionName}</div>
         </a>
       </Link>
       <Link href={props.data.carGoods.sectionLink}>
         <a>
-          <Tab>{props.data.carGoods.sectionName}</Tab>
+          <div className={classes.tab}>{props.data.carGoods.sectionName}</div>
         </a>
       </Link>
-    </Tabs>
+    </div>
   )
 }
 
